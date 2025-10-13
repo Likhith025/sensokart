a6d564a9c866e728a5993e884ee21f81e0166249
Likhith025 <alasyamlikhith2@gmail.com>
2025-10-13 11:34:01 +0530

    Done

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
 app.use(express.json({ limit: '10mb' })); // increase limit if sending large Base64 images
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
